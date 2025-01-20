00e10ac4c85c0d38c4e03596ec994d10f16edba7
고승범 <rhtmdqja4262@naver..com>
2025-01-20 09:46:00 +0900

    feat:component로 변경

---
+++ src/component/body/Skills.jsx
@@ -0,0 +1,13 @@
+const Skills = () => {
+    
+    return (
+        <>
+           <h2>자격증</h2>
+           <ul>
+               <li>SQL D(SQL 개발자) <strong>- 한국데이터산업진흥</strong></li>
+           </ul>
+        </>
+    )
+}
+
+export default Skills;
